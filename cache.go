91c58e8ebb46161a34d527a10421057ad0134818
nikogde <diana.k@ventor.tech@gmail.com>
2022-07-03 17:34:09 +0300

    [ADD] solution with cache

--- cache.go
+++ cache.go
@@ -2,22 +2,54 @@ package cache
 
 import "time"
 
+type Dictionary struct {
+	Value      string
+	Deadline   time.Time
+	CanExpired bool
+}
+
 type Cache struct {
+	Key map[string]Dictionary
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{Key: map[string]Dictionary{}}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Put(key, value string) {
+	c.Key[key] = Dictionary{
+		Value:      value,
+		CanExpired: false,
+	}
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Keys() []string {
+	keys := make([]string, 0)
+	now := time.Now()
+
+	for key, value := range c.Key {
+		if !value.CanExpired || (value.CanExpired && now.After(value.Deadline)) {
+			keys = append(keys, key)
+		}
+	}
+	return keys
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Get(key string) (string, bool) {
+	dict := make(map[string]string)
+
+	for _, val := range c.Keys() {
+		dict[val] = c.Key[val].Value
+	}
+
+	k, ok := dict[key]
+	return k, ok
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.Key[key] = Dictionary{
+		Value:      value,
+		Deadline:   deadline,
+		CanExpired: true,
+	}
 }
